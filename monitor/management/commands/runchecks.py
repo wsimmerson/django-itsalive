9128921d7cc58562fefd6070eedf671e7df7d2d4
Wayne Simmerson <wsimmerson@gmail.com>
2016-02-10 14:44:39 -0500

    overhaul of runchecks

--- monitor/management/commands/runchecks.py
+++ monitor/management/commands/runchecks.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from monitor.models import Host
@@ -12,66 +13,76 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        time_threshold = datetime.now() - timedelta(minutes=4)
-        hosts_to_check = Host.objects.filter(updated__lt=time_threshold)
+        lockfile = os.path.join(settings.BASE_DIR, 'cron.lock')
 
-        for host in hosts_to_check:
-            if os.name == 'nt':
-                proc = subprocess.Popen(['ping', '-n', '3', host.ip4address],
-                                        stdout=subprocess.PIPE)
-            elif os.name == 'posix':
-                proc = subprocess.Popen(['ping', '-c', '3', host.ip4address],
-                                        stdout=subprocess.PIPE)
+        try:
+            if not os.path.exists(lockfile):
+                time_th = datetime.now() - timedelta(minutes=4)
+                hosts_to_check = Host.objects.filter(updated__lt=time_th)
 
-            try:
-                res = proc.communicate()
-            except:
-                pass
+                if os.name == 'posix':
+                    os.mknod(lockfile)
+                elif os.name == 'nt':
+                    open(lockfile, 'w+').close()
 
-            details = res[0].decode('utf-8')
-            host.status_detail = details.replace('\r\n', '<br>')
-            host.status_detail = host.status_detail.replace('\n', '<br>')
+                for host in hosts_to_check:
+                    url = "http://" + settings.ALLOWED_HOSTS[0]
+                    url += reverse('monitor:detail',
+                                   kwargs={'host_id': host.id})
 
-            if '100% packet loss' in details or '100% loss' in details:
-                if host.status == 'UP':
-                    host.status = 'WARNING'
-                    message = "Failed to verify status of " + host.name
-                    message += "<br><br>" + host.status_detail
-                    if settings.EMAIL_NOTIFY:
-                        send_mail('WARNING: ' + host.name,
-                                  '',
-                                  settings.EMAIL_HOST_USER,
-                                  settings.EMAIL_TO,
-                                  html_message=message)
-                    if settings.HIPCHAT_NOTIFY:
-                        hipchat.send(message, 'yellow')
+                    if os.name == 'nt':
+                        proc = subprocess.Popen(['ping', '-n', '3',
+                                                 host.ip4address],
+                                                stdout=subprocess.PIPE)
+                    elif os.name == 'posix':
+                        proc = subprocess.Popen(['ping', '-c', '3',
+                                                 host.ip4address],
+                                                stdout=subprocess.PIPE)
 
-                elif host.status == 'WARNING':
-                    message = "Failed to verify status of " + host.name
-                    message += "<br><br>" + host.status_detail
-                    host.status = 'UNREACHABLE'
-                    if settings.EMAIL_NOTIFY:
-                        send_mail('2nd WARNING: ' + host.name + ' UNREACHABLE',
-                                  '',
-                                  settings.EMAIL_HOST_USER,
-                                  settings.EMAIL_TO,
-                                  html_message=message)
-                    if settings.HIPCHAT_NOTIFY:
-                        hipchat.send(message, 'red')
+                    try:
+                        res = proc.communicate()
+                    except:
+                        pass
 
-            else:
-                host.last_seen = datetime.now()
-                if host.status == 'WARNING' or host.status == 'UNREACHABLE':
-                    host.status = 'UP'
-                    message = "Successfully connected to " + host.name
-                    message += "<br><br>" + host.status_detail
-                    if settings.EMAIL_NOTIFY:
-                        send_mail('RECOVERY: ' + host.name,
-                                  '',
-                                  settings.EMAIL_HOST_USER,
-                                  settings.EMAIL_TO,
-                                  html_message=message)
-                    if settings.HIPCHAT_NOTIFY:
-                        hipchat.send(message, 'green')
+                    details = res[0].decode('utf-8')
+                    host.status_detail = details.replace('\r\n', '<br>')
+                    host.status_detail = host.status_detail.replace('\n', '<br>')
 
-            host.save()
+                    if '100% packet loss' in details or '100% loss' in details:
+                        if host.status == 'UP':
+                            host.status = 'WARNING'
+
+                        elif host.status == 'WARNING':
+                            message = "Failed to verify status of " + host.name
+                            message += "<br><br>" + url
+                            host.status = 'UNREACHABLE'
+                            if settings.EMAIL_NOTIFY:
+                                send_mail('WARNING: ' + host.name + ' UNREACHABLE',
+                                          '',
+                                          settings.EMAIL_HOST_USER,
+                                          settings.EMAIL_TO,
+                                          html_message=message)
+                            if settings.HIPCHAT_NOTIFY:
+                                hipchat.send(message, 'red')
+
+                    else:
+                        host.last_seen = datetime.now()
+                        if host.status == 'WARNING':
+                            host.status = 'UP'
+                            message = "Successfully connected to " + host.name
+                            message += "<br><br>" + url
+                            if settings.EMAIL_NOTIFY:
+                                send_mail('RECOVERY: ' + host.name,
+                                          '',
+                                          settings.EMAIL_HOST_USER,
+                                          settings.EMAIL_TO,
+                                          html_message=message)
+                            if settings.HIPCHAT_NOTIFY:
+                                hipchat.send(message, 'green')
+                        elif host.status == 'UNREACHABLE':
+                            host.status = 'WARNING'
+
+                    host.save()
+                os.remove(lockfile)
+        except:
+            os.remove(lockfile)
